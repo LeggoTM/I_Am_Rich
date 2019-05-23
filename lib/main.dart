e5992266818cdd670af6e17f98e2c219327e5d31
Tanmay Mohapatra <m.tanmay17@gmail.com>
2019-05-23 18:16:47 +0530

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,34 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-  MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text("I Am Rich!"),
-        backgroundColor: Colors.grey[800],
-      ),
-      body: Stack(
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-              gradient: RadialGradient(
-                center: Alignment.center,
-                radius: 1.0,
-                colors: [
-                  Color(0xffffffff),
-                  Color(0xff97a9b7),
-                ],
-              ),
-            ),
+      MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text("I Am Rich!"),
+            backgroundColor: Colors.grey[800],
           ),
-
-        ],
+          body: Stack(
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  gradient: RadialGradient(
+                    center: Alignment.center,
+                    radius: 1.0,
+                    colors: [
+                      Colors.white,
+                      Colors.blueGrey[800],
+                    ],
+                  ),
+                ),
+              ),
+              Center(
+                child: Image(
+                  image: AssetImage('images/asset-diamond.png'),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  ),
-);
+    );
